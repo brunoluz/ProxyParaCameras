9cc23e5c60888ce5d63354bea24569c951aab839
Bruno Luz <bruno.al.luz@gmail.com>
2019-11-15 00:04:24 -0300

    ajustando para novas cameras

--- ProxyParaCameras/Models/UrlCameraHelper.cs
+++ ProxyParaCameras/Models/UrlCameraHelper.cs
@@ -18,27 +18,13 @@ namespace ProxyParaCameras2.Models
             {
                 url = string.Format("http://192.168.0.173:5554/image/IPC02?time={0}", DateTime.Now.ToOADate().ToString().Replace(",", "").Replace(".", ""));
             }
-            else if (string.Compare(camera, "ipcam03", true) == 0)
-            {
-                url = "http://192.168.0.171:5113/snapshot.cgi";
-            }
-            else if (string.Compare(camera, "ipcam04", true) == 0)
-            {
-                url = "http://192.168.0.179:5114/snapshot.cgi?user=casa&pwd=familiaLuz";
-            }
             else if (string.Compare(camera, "ipcam05", true) == 0)
             {
                 url = string.Format("http://192.168.0.173:5554/image/IPC05?time={0}", DateTime.Now.ToOADate().ToString().Replace(",", "").Replace(".", ""));
             }
-            return url;
-        }
-
-        internal string ObterUrlDeCameraParaReset(string camera)
-        {
-            string url = string.Empty;
-            if (string.Compare(camera, "ipcam02", true) == 0)
+            else if (string.Compare(camera, "ipcam06", true) == 0)
             {
-                url = "http://192.168.0.170:5112/reboot.xml";
+                url = string.Format("http://192.168.0.174:5555/image/IPC06?time={0}", DateTime.Now.ToOADate().ToString().Replace(",", "").Replace(".", ""));
             }
             return url;
         }
